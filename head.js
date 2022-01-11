f68bf67ac79129c85ab4d6d2f16ce322072af953
Khadija Mohamed <khadijam@live.ca>
2022-01-10 20:29:50 -0500

    to return first index of array

---
+++ head.js
@@ -0,0 +1,20 @@
+const head = function(array) {
+  if (array.length > 0) {
+    return array[0];
+  } else {
+    return undefined
+  }
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
