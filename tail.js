a82d11de1592853e29625dbe990fa3abac018f38
Khadija Mohamed <khadijam@live.ca>
2022-01-10 20:45:06 -0500

    tail function and eslint fix

---
+++ tail.js
@@ -0,0 +1,18 @@
+const tail = function(array) {
+  let tailArray = array.slice(1);
+  return (console.log(tailArray))
+  return array.slice(1)
+};
+
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1); */
