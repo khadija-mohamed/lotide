c7bc4055b7f5e40dcfa9d42b52c1ada975e7137e
Khadija Mohamed <khadijam@live.ca>
2022-01-12 14:40:25 -0500

    refractoring countLetter function

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const countLetters = function(string) {
+  const results = {}
+  for (const letter of string) {
+    if (letter !== " ") {
+      if (results[letter])
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results
+};
+
