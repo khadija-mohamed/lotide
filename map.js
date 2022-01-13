4618e083e9d186921fd568d20dc2ac02ab945cdc
Khadija Mohamed <khadijam@live.ca>
2022-01-13 17:59:47 -0500

    refractored maps function

---
+++ map.js
@@ -0,0 +1,33 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+  } 
+  return results;
+} 
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+  return false;
+}
+  return true;
+} 
+
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays (actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}; 
