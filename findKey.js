9e5f2e0fd0d257eb78b05e1ad4c36d91f045f5cf
Khadija Mohamed <khadijam@live.ca>
2022-01-13 21:14:47 -0500

    findKey function for short hand call backs

---
+++ findKey.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+function findKey(object,value) {
+  const keys = Object.keys(object);
+    for (let element of keys) {
+    if (object [element] === value)
+      return element
+    }
+  };
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
