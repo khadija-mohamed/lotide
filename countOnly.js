adc3fae198617a57e0b8cde0999d85661da481c2
Khadija Mohamed <khadijam@live.ca>
2022-01-17 18:48:45 -0500

    Implement countOnly function + eslint

--- countOnly.js
+++ countOnly.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
+  } 
+  if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
   }
 };
@@ -19,12 +19,9 @@ const countOnly = function(allItems , itemsToCount) {
     if (itemsToCount[item]) {
      if (results[item]) {
        results[item] += 1;
-     } else {
-       results[item] = 1
-     }
-    }
   }
     return results;
+  }
 }
 
 
@@ -45,4 +42,5 @@ const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": t
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Agouhanna"], undefined); 
+}
