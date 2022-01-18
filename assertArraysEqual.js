672c5bed5afbff3f7db8faf5d216187db8ee7f8f
Khadija Mohamed <khadijam@live.ca>
2022-01-17 19:43:29 -0500

    Refractored implementation of eqArrays function + eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,6 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  } if (array1[i] === array2[i])  {
-    return false;
-  }
-  return true;
-};
+const eqArrays = require ("./eqArrays");
 
-const assertEqual = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
@@ -20,8 +9,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
+module.exports = assertArraysEqual;
