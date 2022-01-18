5951d74e93eba7b4663eaf8e4295ddac29e4d061
Khadija Mohamed <khadijam@live.ca>
2022-01-17 19:46:34 -0500

    Refractored middle function + eslint

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
- 
-  return true
-  }
-}
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } 
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-    }
-  };
-
 const middle = function(array) {
   let arrayOutput = [];
   if (array.length <= 2) {
@@ -34,3 +13,4 @@ const middle = function(array) {
   } return arrayOutput
 };
 
+module.exports = middle;
