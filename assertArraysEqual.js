a6bb67edebc234db4aa2b39ac7b47a63b91fd5eb
Khadija Mohamed <khadijam@live.ca>
2022-01-17 19:23:44 -0500

    Implementation of eqArray function + eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,5 +20,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
 
-
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
